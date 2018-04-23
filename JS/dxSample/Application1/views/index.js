732a3f8e25f162fc5f7a48d99ec951638c3cb1d1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/dxSample/Application1/views/index.js
@@ -0,0 +1,9 @@
+﻿Application1.Index = function(params) {
+
+    var viewModel = {
+        dSource: Application1.db.sampleData,
+        galleryData: Application1.db.galleryData
+    };
+
+    return viewModel;
+};
